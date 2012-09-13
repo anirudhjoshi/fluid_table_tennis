71f7dc572b72f373cc1a38fa06dd801b616bf7a5
Anirudh Joshi <github@anirudhjoshi.com>
2012-09-13 20:33:49 +1000

    wrapped suck draw

--- src/js/main.js
+++ src/js/main.js
@@ -382,6 +382,19 @@ function prepareFrame(field) {
 
 }
 
+function drawSuck() {
+
+	ctx.fillStyle = "black";
+	ctx.fillRect(0,1, canvas.width, 4);		
+
+	ctx.fillStyle = arrayToRGBA( pong.ai.color );
+	ctx.fillRect(1,2, ( canvas.width/ 2 - 2 ) * suck_counter_1 / 100, 2);
+
+	ctx.fillStyle = arrayToRGBA( pong.player.color );	
+	ctx.fillRect(canvas.width / 2 + ( canvas.width/ 2 ) * ( 1 - suck_counter_2 / 100 ),2, ( canvas.width/ 2 - 1 ) * suck_counter_2 / 100, 2);
+
+}
+
 function drawLives(){
 
 		ctx.fillStyle = "black";
@@ -614,18 +627,12 @@ function updateFrame() {
 		}
 
 		field.update();    
+		
 		pong.loop();
+		
 		notify();
 
-		ctx.fillStyle = "black";
-		ctx.fillRect(0,1, canvas.width, 4);		
-
-		ctx.fillStyle = arrayToRGBA( pong.ai.color );
-		ctx.fillRect(1,2, ( canvas.width/ 2 - 2 ) * suck_counter_1 / 100, 2);
-
-		ctx.fillStyle = arrayToRGBA( pong.player.color );	
-		ctx.fillRect(canvas.width / 2 + ( canvas.width/ 2 ) * ( 1 - suck_counter_2 / 100 ),2, ( canvas.width/ 2 - 1 ) * suck_counter_2 / 100, 2);
-
+		drawSuck();
 		drawLives();
 
 		if ( run_coul ){
