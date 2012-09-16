bb852d916c23ddd439cc550050164a37a7a05fcc
Anirudh Joshi <github@anirudhjoshi.com>
2012-09-16 13:49:05 +1000

    reverted

--- utils/build.sh
+++ utils/build.sh
@@ -6,5 +6,4 @@ java -jar compiler.jar --js ../src/js/fluid.js ../src/js/table_tennis.js ../src/
 
 java -jar yuicompressor-2.4.7.jar ../src/css/main.css -o ../build/f.css
 
-#java -jar htmlcompressor-1.5.3.jar --compress-js --js-compressor closure --closure-opt-level advanced ../src/index.html > ../build/index.html
-java -jar htmlcompressor-1.5.3.jar ../src/index.html > ../build/index.html
+java -jar htmlcompressor-1.5.3.jar --compress-js --js-compressor closure --closure-opt-level advanced ../src/index.html > ../build/index.html
